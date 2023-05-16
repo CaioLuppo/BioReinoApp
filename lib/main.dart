4db406b62b35ad732cc5c34a42d4f588588ec591
CaioLuppo <luppodecode@outlook.com>
2023-05-16 15:49:19 -0300

    refactor: runapp virou arrow function

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:bioreino_mobile/view/screens/splash_screen/splash_screen.dart';
 import 'package:bioreino_mobile/view/themes/theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const BioReino());
-}
+void main() => runApp(const BioReino());
 
 class BioReino extends StatelessWidget {
   const BioReino({super.key});
@@ -17,5 +15,4 @@ class BioReino extends StatelessWidget {
       home: const SplashScreen(),
     );
   }
-  
 }
