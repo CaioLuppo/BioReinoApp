c2f006c8a0b9cf50fe14495f1e6478a619780434
CaioLuppo <luppodecode@outlook.com>
2023-05-16 17:48:05 -0300

    feat: esqueleto da home

--- lib/view/screens/pages/home/home.dart
+++ lib/view/screens/pages/home/home.dart
@@ -5,6 +5,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      builder: (context, snapshot) {
+        return Column();
+      },
+    );
   }
+
 }
