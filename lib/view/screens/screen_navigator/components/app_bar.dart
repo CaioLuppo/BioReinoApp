82a2278e1ab40c7fccf8d0fc7e880acafb3e6c9c
CaioLuppo <luppodecode@outlook.com>
2023-05-29 03:58:28 -0300

    style: alteração do button drawer

--- lib/view/screens/screen_navigator/components/app_bar.dart
+++ lib/view/screens/screen_navigator/components/app_bar.dart
@@ -4,6 +4,21 @@ final AppBar appBar = AppBar(
   toolbarHeight: 86,
   elevation: 0,
   centerTitle: true,
+  leading: Builder(
+    builder: (context) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 16.0),
+        child: IconButton(
+          onPressed: () => Scaffold.of(context).openDrawer(),
+          icon: SvgPicture.asset(
+            "assets/drawer_icons/drawer_button_icon.svg",
+            height: 24,
+          ),
+          tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+        ),
+      );
+    },
+  ),
   title: Padding(
     padding: const EdgeInsets.only(left: 0.0),
     child: SvgPicture.asset(
