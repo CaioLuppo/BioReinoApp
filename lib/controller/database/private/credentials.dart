fd8909f66887215a32f5d4c5ec51202e5f6aecf1
CaioLuppo <luppodecode@outlook.com>
2023-06-27 22:36:09 -0300

    fix: visitante mongodb

--- lib/controller/database/private/credentials.dart
+++ lib/controller/database/private/credentials.dart
@@ -1,7 +1,7 @@
 const apiKey =
     "Dm8VWfFh2EvVYmVkXH9BlewMIw02gXcjYjG63rbP3kxLrdbimu8JO03jv7HRzmLs";
-const user = "Caio";
-const pass = "Luppo11";
+const user = "Visitante";
+const pass = "NLTnH30orrMLaW7z";
 const mongoUrl =
     "mongodb+srv://$user:$pass@bioreino.l8j1rrn.mongodb.net/bioreino?retryWrites=true&w=majority";
 const coursesCollectionName = "courses";
